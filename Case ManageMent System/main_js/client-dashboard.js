d627b6ee7bc3a082c22cb417783df4d6ba01d41b
sonam choudhari <sonam.cho346@gmail.com>
2024-01-26 12:54:31 +0000

    crm form

--- Case ManageMent System/main_js/client-dashboard.js
+++ Case ManageMent System/main_js/client-dashboard.js
@@ -392,6 +392,12 @@ $(document).ready(function () {
   $(document).on("click", "#CreateNewTask", function () { 
     $("#CreateNewTask_modal").modal("show");
   });
+  $(document).on("click", "#CreateNewWPSTask", function () { 
+    $("#CreateNewWPSTask_modal").modal("show");
+  });
+  $(document).on("click", "#CreateNewTimingTask", function () { 
+    $("#CreateNewTimingTask_modal").modal("show");
+  });
   $(document).on("click", "#tab-5", function () {
     console.log("showdata");
     showAlltask(Alltask_data);
@@ -1749,60 +1755,136 @@ $(document).ready(function () {
   $(document).on("click", ".CList", function () {
     setNotesEditor();
 });
+$(document).on("click", ".CList_wps", function () {
+  setNotesEditor_wps();
+});
 try{
   var editorBtn = document.getElementById('editorBtn');
-	var element = document.getElementById('editor');
-
+	var element =   document.getElementById('editor');
+  let element1 =  document.getElementById('editor1');
 	editorBtn.addEventListener('click', function(e) {
 	  e.preventDefault();
 
 	  if (element.isContentEditable) {
 	    // Disable Editing
 	    element.contentEditable = 'false';
+      element1.contentEditable='false';
 	    editorBtn.innerHTML = 'Enable Editing';
 	    // You could save any changes here.
 	  } else {
 	    element.contentEditable = 'true';
+      element1.contentEditable='true';
 	    editorBtn.innerHTML = 'Disable Editing';
 	  }
 	});
 }catch (e){}
+let markup1 = "this is a comment"
 function setNotesEditor() {
-  //value= markup
-  let editor = $(".html-editor").dxHtmlEditor({
-      height: 200,
-      value: markup,
-      toolbar: {
-          items: [
-              "undo", "redo", "separator",
-              {
-                  name: "size",
-                  acceptedValues: ["8pt", "10pt", "12pt", "14pt", "18pt", "24pt", "36pt"]
-              },
-              {
-                  name: "font",
-                  acceptedValues: ["Arial", "Courier New", "Georgia", "Impact", "Lucida Console", "Tahoma", "Times New Roman", "Verdana"]
-              },
-              "separator", "bold", "italic", "strike", "underline", "separator",
-              "alignLeft", "alignCenter", "alignRight", "alignJustify", "separator",
-              "orderedList", "bulletList", "separator",
-              {
-                  name: "header",
-                  acceptedValues: [false, 1, 2, 3, 4, 5]
-              }, "separator",
-              "color", "background", "separator",
-              "link", "image", "separator",
-              "clear", "codeBlock", "blockquote", "separator",
-              "insertTable", "deleteTable",
-              "insertRowAbove", "insertRowBelow", "deleteRow",
-              "insertColumnLeft", "insertColumnRight", "deleteColumn"
-          ]
-      },
-      mediaResizing: {
-          enabled: true
-      }
-  }).dxHtmlEditor("instance");
+  ClassicEditor
+  .create( document.querySelector( '#editors' ))
+  .catch( error => {
+      console.error( error );
+  } );
+  // CKEDITOR.replace('editors', {
+  //   skin: 'moono',
+  //   enterMode: CKEDITOR.ENTER_BR,
+  //   shiftEnterMode:CKEDITOR.ENTER_P,
+  //   toolbar: [{ name: 'basicstyles', groups: [ 'basicstyles' ], items: [ 'Bold', 'Italic', 'Underline', "-", 'TextColor', 'BGColor' ] },
+  //              { name: 'styles', items: [ 'Format', 'Font', 'FontSize' ] },
+  //              { name: 'scripts', items: [ 'Subscript', 'Superscript' ] },
+  //              { name: 'justify', groups: [ 'blocks', 'align' ], items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+  //              { name: 'paragraph', groups: [ 'list', 'indent' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent'] },
+  //              { name: 'links', items: [ 'Link', 'Unlink' ] },
+  //              { name: 'insert', items: [ 'Image'] },
+  //              { name: 'spell', items: [ 'jQuerySpellChecker' ] },
+  //              { name: 'table', items: [ 'Table' ] }
+  //              ],
+  // });
+  
 }
+
+
+function setNotesEditor_wps() {
+  ClassicEditor
+  .create( document.querySelector( '#editors_wps' ) )
+  .catch( error => {
+      console.error( error );
+  } );
+  // CKEDITOR.replace('editors', {
+  //   skin: 'moono',
+  //   enterMode: CKEDITOR.ENTER_BR,
+  //   shiftEnterMode:CKEDITOR.ENTER_P,
+  //   toolbar: [{ name: 'basicstyles', groups: [ 'basicstyles' ], items: [ 'Bold', 'Italic', 'Underline', "-", 'TextColor', 'BGColor' ] },
+  //              { name: 'styles', items: [ 'Format', 'Font', 'FontSize' ] },
+  //              { name: 'scripts', items: [ 'Subscript', 'Superscript' ] },
+  //              { name: 'justify', groups: [ 'blocks', 'align' ], items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+  //              { name: 'paragraph', groups: [ 'list', 'indent' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent'] },
+  //              { name: 'links', items: [ 'Link', 'Unlink' ] },
+  //              { name: 'insert', items: [ 'Image'] },
+  //              { name: 'spell', items: [ 'jQuerySpellChecker' ] },
+  //              { name: 'table', items: [ 'Table' ] }
+  //              ],
+  // });
+  
+}
+
+
+
+
+const markup = `
+    <h2>
+        <img src="https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/images/widgets/HtmlEditor.svg" alt="HtmlEditor">
+        Formatted Text Editor (HTML Editor)
+    </h2>
+    <br>
+    <p>DevExtreme JavaScript HTML Editor is a client-side WYSIWYG text editor that allows its users to format textual and visual content and store it as HTML or Markdown.</p>
+    <p>Supported features:</p>
+    <ul>
+        <li>Inline formats:
+            <ul>
+                <li><strong>Bold</strong>, <em>italic</em>, <s>strikethrough</s> text formatting</li>
+                <li>Font, size, color changes (HTML only)</li>
+            </ul>
+        </li>
+        <li>Block formats:
+            <ul>
+                <li>Headers</li>
+                <li>Text alignment</li>
+                <li>Lists (ordered and unordered)</li>
+                <li>Code blocks</li>
+                <li>Quotes</li>
+            </ul>
+        </li>
+        <li>Custom formats</li>
+        <li>HTML and Markdown support</li>
+        <li>Mail-merge placeholders (for example, %username%)</li>
+        <li>Adaptive toolbar for working images, links, and color formats</li>
+        <li>Insert images as a link or base64 (drag and drop images to convert them to base64)</li>
+        <li>Copy-paste rich content (unsupported formats are removed)</li>
+        <li>Tables support</li>
+    </ul>
+    <br>
+    <p>Supported frameworks and libraries</p>
+    <table>
+        <tr>
+            <td><strong>jQuery</strong></td>
+            <td style="text-align: right;">v2.1 - v2.2 and v3.x</td>
+        </tr>
+        <tr>
+            <td><strong>Angular</strong></td>
+            <td style="text-align: right;">v7.0.x - v10.0.x</td>
+        </tr>
+        <tr>
+            <td><strong>React</strong></td>
+            <td style="text-align: right;">v16.2+</td>
+        </tr>
+        <tr>
+            <td><strong>Vue</strong></td>
+            <td style="text-align: right;">v2.6.3+</td>
+        </tr>
+    </table>
+`;
+
    //start Get Forward User List
    function getInitialsFromName(name) {
     const words = name.split(' ');
